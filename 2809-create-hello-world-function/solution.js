1eea70233379d077bfe7e7ec53b22f3faadd15ed
Lê Hiển Vinh <zin.it.dev@gmail.com>
2026-06-13 14:43:42 +0000

    [LeetCode] Runtime - 41 ms (63.52%), Memory - 54 MB (18.97%)

--- 2809-create-hello-world-function/solution.js
+++ 2809-create-hello-world-function/solution.js
@@ -2,10 +2,13 @@
  * @return {Function}
  */
 var createHelloWorld = function() {
+    
     return function(...args) {
         return "Hello World"
     }
 };
 
- const f = createHelloWorld();
- f(); // "Hello World"
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
